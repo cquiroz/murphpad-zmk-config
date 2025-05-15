4d45e77a7bd45fd2bc8621a317410bd7b5860b68
Carlos Quiroz <3615303+cquiroz@users.noreply.github.com>
2025-05-15 12:40:32 -0400

    Some updates

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-python }:
+    let
+      #system = "x86_64-linux";
+      system = "aarch64-darwin";
+
+      pythonVersion = "3.10.1";
+
+
+      pkgs = import nixpkgs { inherit system; };
+      myPython = nixpkgs-python.packages.${system}.${pythonVersion};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          myPython
+        ];
+        shellHook = ''
+          python --version
+        '';
+      };
+    };
+}
